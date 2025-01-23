4fc4cf03b3e06bb52ea3ecec2a5963c1a5116e35
Filip Akkad <54677165+filipakkad@users.noreply.github.com>
2025-01-23 15:08:52 +0100

    next

--- src/validate_json_schema.cpp
+++ src/validate_json_schema.cpp
@@ -1,6 +1,6 @@
 #include <Rcpp.h>
 #include <nlohmann/json.hpp>
-#include "json-schema-validator/nlohmann/json-schema.hpp"
+#include "include/json-schema-validator/include/nlohmann/json-schema.hpp"
 
 // [[Rcpp::export]]
 bool validate_json_schema(
